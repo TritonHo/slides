7b62d71ccf65816b75249f18c739c5ac035884f0
Triton Ho <>
2020-09-20 22:53:35 +0800

    finish the qsort good

--- Kaohsiung 2020-10 talk/qsort_good.go
+++ Kaohsiung 2020-10 talk/qsort_good.go
@@ -1,24 +1,47 @@
 package qsort
 
-/*
 import "sync"
 
-func qsortGoodWorker(inputCh chan []input, wg *sync.WaitGroup) {
+func qsortGoodWorker(inputCh chan []int, wg *sync.WaitGroup, remainingTaskNum *sync.WaitGroup) {
 	defer wg.Done()
 
 	for input := range inputCh {
+		// end condition of recursion
 		if len(input) <= 1 {
+			remainingTaskNum.Done()
 			continue
 		}
 
 		pivotPos := qsortPartition(input)
+
+		// add the sub-tasks to the queue
+		remainingTaskNum.Add(2)
 		inputCh <- input[:pivotPos]
 		inputCh <- input[pivotPos+1:]
+
+		// mark the current task is done
+		remainingTaskNum.Done()
 	}
 }
 
 func qsortGood(input []int) {
 	wg := sync.WaitGroup{}
+	remainingTaskNum := sync.WaitGroup{}
 
+	numOfThreads := 4
+
+	inputCh := make(chan []int, 100)
+	wg.Add(numOfThreads)
+	for i := 0; i < numOfThreads; i++ {
+		go qsortGoodWorker(inputCh, &wg, &remainingTaskNum)
+	}
+
+	// add the input to channel, and wait for all subtask completed
+	remainingTaskNum.Add(1)
+	inputCh <- input
+	remainingTaskNum.Wait()
+
+	// let worker thread die peacefully, we SHOULD NOT leave the worker thread behind
+	close(inputCh)
+	wg.Wait()
 }
-*/
